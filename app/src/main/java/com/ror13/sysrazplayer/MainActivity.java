c9dc945c5127e463613d2fda9244798f8e411ea8
ror13 <kapashilov@gmailr.com>
2015-12-09 12:22:25 +0300

    add file dialog

--- app/src/main/java/com/ror13/sysrazplayer/MainActivity.java
+++ app/src/main/java/com/ror13/sysrazplayer/MainActivity.java
@@ -2,6 +2,10 @@ package com.ror13.sysrazplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import java.io.*;
+import android.os.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.e("tst debug:::", "1");
+        super.onTouchEvent(event);
+        Log.e("tst debug:::", "2");
+
+        FileDialog fileDialog = new FileDialog( );
+        Log.e("tst debug:::", fileDialog.toString());
+        return true;
     }
 }
