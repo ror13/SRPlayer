79f98d5b51da138c1b2ebe350adc0a83dfb69f29
Eugen <eugen@bigbyte.com>
2016-01-05 18:52:54 +0300

    fixed build

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -31,8 +31,8 @@ LOCAL_ALLOW_UNDEFINED_SYMBOLS=false
 LOCAL_MODULE := CFfmpeg
 LOCAL_SRC_FILES := CFfmpeg.cpp
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-build/$(TARGET_ARCH_ABI)/include
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
-LOCAL_LDFLAGS := -llog  -landroid  -lz -lm -lutils -lstagefright  -g $(LOCAL_PATH)/ffmpeg-build/$(TARGET_ARCH_ABI)/libffmpeg-neon.so -L$(LOCAL_PATH)/lib
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/dependencies/include
+LOCAL_LDFLAGS := -llog  -landroid  -lz -lm -lutils -lstagefright  -g $(LOCAL_PATH)/ffmpeg-build/$(TARGET_ARCH_ABI)/libffmpeg-neon.so -L$(LOCAL_PATH)/dependencies/lib
 #-L$(LOCAL_PATH)/ffmpeg-build/$(TARGET_ARCH_ABI)
 LOCAL_SHARED_LIBRARY := ffmpeg-prebuilt
 include $(BUILD_SHARED_LIBRARY)
